8e69cfba1ed85e8b1e4977d805f4e19a5dc8413a
nhocratac <22521339@gm.uit.edu.vn>
2024-03-29 20:02:30 +0700

    thắng commit (them button cho khoahocpage

--- src/Pages/Khoahoc/KhoahocItem.jsx
+++ src/Pages/Khoahoc/KhoahocItem.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-const KhoahocItem = ({KhoaHoc}) => { // tạo props với object
-  return (
-    <div className="border-2 ml-[100px] wrap">
-      <h1>title className: {KhoaHoc.ten}</h1>
-        <div>  {/* content của môn học*/}
-        <p>GVTH: {KhoaHoc.giaovien}</p>
-        <p>Sĩ số: {KhoaHoc.siso}</p>
-        </div>  
-    </div>
-  );
+const KhoahocItem = ({KhoaHoc,Tinhtrang}) => { // tạo props với object
+  console.log(Tinhtrang)
+  if (Tinhtrang == 2 || Tinhtrang == KhoaHoc.tinhtrang)
+  {
+    return (
+      <div className="border-2 ml-[100px] wrap">
+        <h1>title className: {KhoaHoc.ten}</h1>
+          <div>  {/* content của môn học*/}
+          <p>GVTH: {KhoaHoc.giaovien}</p>
+          <p>Sĩ số: {KhoaHoc.siso}</p>
+          </div>  
+      </div>
+    );
+  }
 };
 // tạo component KhoahocItem với props ten, giaovien, siso và trả về 1 div chứa title, giáo viên và sĩ số
 export default KhoahocItem;
--- src/Pages/Khoahocpage.jsx
+++ src/Pages/Khoahocpage.jsx
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import KhoahocItem from "./Khoahoc/KhoahocItem";
+import { Link, Route } from "react-router-dom";
 
-const khoahoc=[   // tạo mảng lưu dữ liệu của các lơp học
-  {giaovien:"Thay Thanh1",ten:"React",siso:20},
-  {giaovien:"Thay Thanh2",ten:"React",siso:20},
-  {giaovien:"Thay Thanh3",ten:"React",siso:20},
-  {giaovien:"Thay Thanh4",ten:"React",siso:20},
-  {giaovien:"thầy thắng",ten:"React-wed",siso:100},
-  ]
+const khoahoc = [
+  { giaovien: "Thay Thanh1", ten: "React", siso: 20, tinhtrang: 1 },
+  { giaovien: "Thay Thanh2", ten: "React", siso: 20, tinhtrang: 1 },
+  { giaovien: "Thay Thanh3", ten: "React", siso: 20, tinhtrang: 0 },
+  { giaovien: "Thay Thanh4", ten: "React", siso: 20, tinhtrang: 0 },
+  { giaovien: "thầy thắng", ten: "React-wed", siso: 100, tinhtrang: 1 },
+];
 
-const Khoahocpage = () => {     // component khóa học page
+const Khoahocpage = () => {
+  const [filter, setFilter] = useState(2); // Default filter: all (2)
+
+  const handleFilterChange = (value) => {
+    setFilter(value);
+  };
+
   return (
     <div>
-      <div className="relative ml-[200px] flex">  {/* container */}
-        {
-            khoahoc.map((khoahoc,index) =>
-            {
-              return (
-              <KhoahocItem 
-                key = {index} KhoaHoc = {khoahoc}
-               
-                 />);
-            })}
+      <div className="relative ml-[200px]">
+        <ul className="flex">
+          <li className="m-2" onClick={() => handleFilterChange(2)}>
+            Tất cả
+          </li>
+          <li className="m-2" onClick={() => handleFilterChange(1)}>
+            Đang học
+          </li>
+          <li className="m-2" onClick={() => handleFilterChange(0)}>
+            Đã học
+          </li>
+        </ul>
+        <div className="flex">
+          {khoahoc
+            .filter((item) => filter === 2 || item.tinhtrang === filter)
+            .map((khoahoc, index) => (
+              <div key={index}>
+                <KhoahocItem KhoaHoc={khoahoc} Tinhtrang={filter} />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
