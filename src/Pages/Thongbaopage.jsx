873beba5d09ca815b05e2e079d7ce028ec773d3d
nhocratac <22521339@gm.uit.edu.vn>
2024-04-19 21:18:40 +0700

    clear branch

--- src/Pages/Thongbaopage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Thongbaopage = () => {
-  return <div></div>;
-};
-
-export default Thongbaopage;
--- src/Pages/Tinnhanpage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Tinnhanpage = () => {
-  return <div></div>;
-};
-
-export default Tinnhanpage;
