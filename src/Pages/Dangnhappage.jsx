0dae350fc36df69fa7518dc2204476ab978aea6e
nhocratac <22521339@gm.uit.edu.vn>
2024-04-25 22:06:50 +0700

    validate form

--- src/Pages/Dangnhappage.jsx
+++ src/Pages/Dangnhappage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import loginImg from '../assets/BackgroundLogin.svg'
 import { FaRegEyeSlash } from "react-icons/fa6"
 
+import validator from '../hook/validate'
+
 const Dangnhappage = () => {
+    useEffect(() => {
+        
+        validator({
+            form: '#form-login',
+            formGroup: '.form-group',
+            errorMessage: '.form-message',
+            styleInvalid: 'border-red-500' ,
+            rules: [
+                validator.isRequired('#email', 'Vui lòng nhập email'),
+                validator.isEmail('#email', 'Email không hợp lệ'),
+                validator.isRequired('#password', 'Vui lòng nhập mật khẩu'),
+                validator.minLength('#password', 6),
+            ],
+        });
+    }, [])
+
     return (
         <div className='flex bg-[#F0F7FF] flex-col items-center justify-center  flex-1 px-20 text-center'>
             <main className='flex  flex-col items-center w-full flex-1 px-20 text-center  mt-9'>
@@ -10,16 +28,18 @@ const Dangnhappage = () => {
                     <div className='md:w-4/5 p-6'>
                         <div className=' py-8'>
                             <img className='px-24 ' src="./src/assets/LogoUIT.svg" alt="logiUIT" />
-                            <h2 className='text-3xl mt-2 font-bold text-black mb-2'>Đăng nhập</h2>
+                            <button className='text-3xl mt-2 font-bold text-black mb-2 form-submit'>Đăng nhập</button>
                         </div>
                         <div>
-                            <form className='flex flex-col gap-2' id='form-dangnhap'>
+                            <form className='flex flex-col gap-2 form-group' id='form-login'>
                                 <div>
-                                    <input className='p-2 rounded-xl border' type="email" name="email" placeholder='Email' />
+                                    <input className='p-2 rounded-xl border' type="email" id='email' name="email" placeholder='Email' />
+                                    <span className="form-message block  text-red-500"></span>
                                 </div>
-                                <div className='relative'>
-                                    <input className='p-2 mt-2 rounded-xl border' type="password" name="password" placeholder='Mật khẩu' />
+                                <div className='relative form-group'>
+                                    <input className='p-2 mt-2 rounded-xl border' type="password" id="password" name="password" placeholder='Mật khẩu' />
                                     <FaRegEyeSlash className='absolute fill-gray-400 top-7 right-3 -translate-y-1/2 inline-block w-5 h-5 mr-8 ' />
+                                    <span className="form-message block  text-red-500"></span>
                                 </div>
                                 <div className='text-sm mr-10'>
                                     <p ><input type="checkbox" />Nhớ tài khoản của tôi</p>
