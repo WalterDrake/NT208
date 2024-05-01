63b00356b989a5ad0eee3982c4e5396074a3ac73
Đạt Nguyễn Phan Quốc <22520218@ms.uit.edu.vn>
2024-05-01 15:06:55 +0700

    update

--- src/routes/Api/commentRoute.js
+++ src/routes/Api/commentRoute.js
@@ -1,23 +1,23 @@
-import express from "express";
-import { StatusCodes } from "http-status-codes";
-import { cboxController } from "~/controllers/commentBoxController";
-import { commentController } from "~/controllers/commentController";
-import { postController } from "~/controllers/postController";
-import { cboxModel } from "~/models/Monhoc/commentboxModel";
-import { cboxValidation } from "~/validations/commentBoxValidation";
-import { commentValidation } from "~/validations/commentValidation";
-import { postValidation } from "~/validations/postValidation";
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { cboxController } from '~/controllers/commentBoxController'
+import { commentController } from '~/controllers/commentController'
+import { postController } from '~/controllers/postController'
+import { cboxModel } from '~/models/Monhoc/commentboxModel'
+import { cboxValidation } from '~/validations/commentBoxValidation'
+import { commentValidation } from '~/validations/commentValidation'
+import { postValidation } from '~/validations/postValidation'
 
-const Router = express.Router();
+const Router = express.Router()
 
-Router.route("/")
+Router.route('/')
   .get((req, res) => {
-    res.status(StatusCodes.OK).json({ message: "GET: API get list comemnt" });
+    res.status(StatusCodes.OK).json({ message: 'GET: API get list comemnt' })
   })
-  .post(commentValidation.createNew, commentController.createNew);
+  .post(commentValidation.createNew, commentController.createNew)
 
-Router.route("/:id").get(commentController.findOneById);
+Router.route('/:id').get(commentController.findOneById)
 
 // API hỗ trợ việc di chuyển card giữa các column khác nhau trong một board
 
-export const commentRoute = Router;
+export const commentRoute = Router
--- src/routes/Api/studyRoute.js
+++ src/routes/Api/studyRoute.js
@@ -1,26 +1,26 @@
-import express from "express";
-import { StatusCodes } from "http-status-codes";
-import { postController } from "~/controllers/postController";
-import { studyController } from "~/controllers/studyController";
-import { postValidation } from "~/validations/postValidation";
-import { studyValidation } from "~/validations/studyValidation";
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { postController } from '~/controllers/postController'
+import { studyController } from '~/controllers/studyController'
+import { postValidation } from '~/validations/postValidation'
+import { studyValidation } from '~/validations/studyValidation'
 
-const Router = express.Router();
+const Router = express.Router()
 
-Router.route("/")
+Router.route('/')
   .get((req, res) => {
-    res.status(StatusCodes.OK).json({ message: "GET: API get list Study" });
+    res.status(StatusCodes.OK).json({ message: 'GET: API get list Study' })
   })
-  .post(studyValidation.createNew, studyController.createNew);
+  .post(studyValidation.createNew, studyController.createNew)
 
-Router.route("/:id")
+Router.route('/:id')
   .get(studyController.getDetailsAll)
-  .put(studyValidation.updateStudy, studyController.updateStudy);
+  .put(studyValidation.updateStudy, studyController.updateStudy)
 
-Router.route("/get/All")
+Router.route('/get/All')
   .get(studyController.getDetailsAll)
-  .put(studyValidation.updateStudy, studyController.updateStudy);
+  .put(studyValidation.updateStudy, studyController.updateStudy)
 
 // API hỗ trợ việc di chuyển card giữa các column khác nhau trong một board
 
-export const studyRoute = Router;
+export const studyRoute = Router
