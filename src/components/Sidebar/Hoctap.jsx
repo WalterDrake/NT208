34ab8b83ac37004a17d806d160c3dd9dd46aa75e
Sukem <thuytranggd11@gmail.com>
2024-05-03 03:15:10 +0700

    updateSidebar

--- src/components/Sidebar/Hoctap.jsx
+++ src/components/Sidebar/Hoctap.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { TbSchool } from "react-icons/tb"
 const Hoctap = () => {
   return (
-    <div className="mb-6 w-[145px] rounded hover:shadow hover:bg-[#F0F7FF] py-2">
+    <div className="mb-6 text-lg w-[135px] rounded ">
       {/* <TbSchool className="inline-block w-6 h-6 mr-2 -mt-2"></TbSchool> */}
       Học tập
     </div>
