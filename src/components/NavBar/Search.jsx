0592f565316f2b7900e73947870a022478cb24a8
nhocratac <22521339@gm.uit.edu.vn>
2024-05-06 22:18:17 +0700

    search lỗi không làm nổi

--- src/components/NavBar/Search.jsx
+++ src/components/NavBar/Search.jsx
@@ -1,13 +1,16 @@
-import {FaSearch} from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import { useEffect, useState, useRef } from 'react';
-import  Tooltip  from "@mui/material/Tooltip";
-import  useDebounce  from "../../hook/useDebounce";
+import Tooltip from "@mui/material/Tooltip";
+import useDebounce from "../../hook/useDebounce";
+import Tippy from "@tippyjs/react";
 
-export  function Search() {
+export function Search() {
     const [searchValue, setsearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     const debouncedValue = useDebounce(searchValue, 500);
+    const [loading, setLoading] = useState()
     const [showResult, setShowResult] = useState(false);
+    const inputRef = useRef(null);
 
     function handleChange(e) {
         const searchValue = e.target.value;
@@ -40,38 +43,26 @@ export  function Search() {
         setSearchResult([]);
         inputRef.current.focus();
     }
-    
+
     function handleHideResult() {
         setShowResult(false);
     }
-
-
-  return (
-   <div>
-        {/* <Tippy
-            interactive
-            visible={showResult && searchResult.length > 0}
-            render={(attrs) => (
-                <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                    <ul>
-                        {Array.isArray(searchResult) && searchResult.map((item) => (
-                            item && item.id && item.name && <li key={item.id}>{item.name}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-            onClickOutside={handleHideResult}
-        > */}
-            <input
-                type="text"
-                className="mr-16 w-full px-4 py-2 shadow-lg rounded-3xl border-2 border-gray-300 pl-10 trancate outline-none"
-                placeholder="Tìm kiếm Khóa học, Tài liệu, Môn học,..."
-                onChange={handleChange}
-                onFocus={() => setShowResult(true)}
-                
-            />
-            <FaSearch className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
-        {/* </Tippy> */}
-   </div>
-  )
+    return (
+        <div>
+            <Tooltip title="hien khong ket noi duoc mang" open={showResult}>
+                <>
+                    <input
+                        type="text"
+                        ref={inputRef}
+                        className="mr-16 w-full px-4 py-2 shadow-lg rounded-3xl border-2 border-gray-300 pl-10 trancate outline-none"
+                        placeholder="Tìm kiếm Khóa học, Tài liệu, Môn học,..."
+                        onChange={handleChange}
+                        onBlur={() => setShowResult(false)}
+                        onFocus={() => setShowResult(true)}
+                    />
+                    <FaSearch className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
+                </>
+            </Tooltip>
+        </div>
+    )
 }
