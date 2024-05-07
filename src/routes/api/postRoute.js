487dc853db7c4c9e2dba96aef752a99f91b6e575
Đạt Nguyễn Phan Quốc <22520218@ms.uit.edu.vn>
2024-05-07 14:05:40 +0700

    update

--- src/routes/api/postRoute.js
+++ src/routes/api/postRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { postController } from '~/controllers/postController'
-
+import { postValidation } from '~/validations/postValidation'
 
 const Router = express.Router()
 
-Router.route('/').get((req, res) => {
+Router.route('/')
+  .get((req, res) => {
   res.status(StatusCodes.OK).json({ message: 'GET: API get list Post' })
 })
 Router.route('/GetPostAll').get(postController.getDetailsAllPost)
