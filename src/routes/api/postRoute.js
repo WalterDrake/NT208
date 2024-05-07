25f90efea5488f635c5c27f1312f4804d0380c9e
Ho Dang Manh Hung <22520502@gm.uit.edu.vn>
2024-05-07 10:13:22 +0700

    "Them Them Them"

--- src/routes/api/postRoute.js
+++ src/routes/api/postRoute.js
@@ -5,14 +5,15 @@ import { postValidation } from "~/validations/postValidation";
 
 const Router = express.Router();
 
-Router.route("/").get((req, res) => {
-  res.status(StatusCodes.OK).json({ message: "GET: API get list Post" });
-});
-Router.route("/GetPostAll").get(postController.getDetailsAllPost);
-Router.route("/CreateNewPost").post(postController.createNewPostOfItem); // truyen data
-Router.route("/UpdatePost/:id").post(postController.updatePostOfItem); // truyen post id va updateData
-Router.route("/DeletePost/:id").delete(postController.deletePostOfItem); // truyen id post
-Router.route("/Getlist/:id").delete(postController.getListPostOfItem); // truyen id item
+Router.route("/")
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ message: "GET: API get list Post" });
+  })
+  .post(postValidation.createNew, postController.createNew);
+
+Router.route("/:id")
+  .get(postController.getDetails)
+  .put(postValidation.updatePost, postController.updatePost);
 
 // API hỗ trợ việc di chuyển card giữa các column khác nhau trong một board
 
