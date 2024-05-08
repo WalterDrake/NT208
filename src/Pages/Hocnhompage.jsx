b115b7a223faec8ff5988a527357f6d881bf70fe
nhocratac <22521339@gm.uit.edu.vn>
2024-05-08 09:57:19 +0700

    -no change

--- src/Pages/Hocnhompage.jsx
+++ src/Pages/Hocnhompage.jsx
@@ -9,60 +9,60 @@ import useUser from "../hook/useUser";
 export const ShowFormAddGroupContext = createContext()
 
 const HocNhompage = () => {
-   const {user} = useUser()
-  const [hocnhoms,setHocnhoms] = useState([])
-  useEffect(()=>{
+  const { user } = useUser()
+  const [hocnhoms, setHocnhoms] = useState([])
+  useEffect(() => {
     groups.getAllGroupByIdUser(user._id)
-    .then((res) => {
-      setHocnhoms(res)
-    })
-    .catch ((err) => {
-      console.log('err get list group',err)
-    })
+      .then((res) => {
+        setHocnhoms(res)
+      })
+      .catch((err) => {
+        console.log('err get list group', err)
+      })
   }
-  ,[])
+    , [])
   const [openAddGroup, setOpenAddGroup] = useState(false);
   const [openJoinGroup, setOpenJoinGroup] = useState(false);
 
   return (
-    <div>
-      <div className="relative ml-[1rem] ">
+    <div className="relative ml-[1rem] w-full ">
+      <div className=" flex flex-col  md:flex-row justify-between w-full">
         <h1 className="text-3xl mb-5 ml-2 font-bold">Học nhóm</h1>
-        <div className="end-3 md:float-right">
-          <ShowFormAddGroupContext.Provider value={{openAddGroup, setOpenAddGroup,openJoinGroup, setOpenJoinGroup}}><HocNhom_Setting /></ShowFormAddGroupContext.Provider>
-        </div>
-        <div className="flex justify-between">
-          {" "}
-          {/* header của hocnhompage */}
-          <ul className="flex mb-5">
-            <li
-              className="m-2 font-bold effect " >
-              Tất cả
-            </li>
-            <li
-              className="m-2 font-bold effect " >
-              Lớp học
-            </li>
-            <li
-              className="m-2 font-bold effect" >
-              Nhóm riêng tư
-            </li>
-          </ul>
-        </div>
-        <div className="flex justify-center">
-            {openAddGroup && <GroupActions.CreateGroup />}
-            {openJoinGroup && <GroupActions.JoinGroup />}
-        </div>
-        <div className="container flex">
-          { hocnhoms.map((HocNhom, index) => (
-            <div key={index} className="item">
-              <HocNhomItem HocNhom={HocNhom}  />
-            </div>
-          ))}
+        <div className="end-3 m-4 md:mr-[50px]">
+          <ShowFormAddGroupContext.Provider value={{ openAddGroup, setOpenAddGroup, openJoinGroup, setOpenJoinGroup }}><HocNhom_Setting /></ShowFormAddGroupContext.Provider>
         </div>
       </div>
+      <div className="flex justify-between">
+        {" "}
+        {/* header của hocnhompage */}
+        <ul className="flex mb-5">
+          <li
+            className="m-2 font-bold effect " >
+            Tất cả
+          </li>
+          <li
+            className="m-2 font-bold effect " >
+            Lớp học
+          </li>
+          <li
+            className="m-2 font-bold effect" >
+            Nhóm riêng tư
+          </li>
+        </ul>
+      </div>
+      <div className="flex justify-center">
+        {openAddGroup && <GroupActions.CreateGroup />}
+        {openJoinGroup && <GroupActions.JoinGroup />}
+      </div>
+      <div className="container flex">
+        {hocnhoms.map((HocNhom, index) => (
+          <div key={index} className="item">
+            <HocNhomItem HocNhom={HocNhom} />
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 };
 
 export default HocNhompage
