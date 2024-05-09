9c907ae37df668f6862b074f08985ba55ce13b70
nhocratac <22521339@gm.uit.edu.vn>
2024-05-09 11:02:43 +0700

    'nice

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
-export const WHITELIST_DOMAINS = ['https://trello-web-mu.vercel.app']
+export const WHITELIST_DOMAINS = ['https://trello-web-mu.vercel.app',
+'http://localhost:5173'
+]
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
