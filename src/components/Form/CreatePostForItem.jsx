b4b09d565118be8382b29ac06b49ed662eecb97f
Hồ Đăng Mạnh Hưng <117001966+manhhunghoo@users.noreply.github.com>
2024-05-11 12:40:28 +0700

    -ok createpost for item

--- src/components/Form/CreatePostForItem.jsx
+++ src/components/Form/CreatePostForItem.jsx
@@ -1,42 +1,51 @@
-import React, { useEffect } from 'react'
-import validator from '../../hook/validate'
+import React, { useEffect } from "react";
+import validator from "../../hook/validate";
 export default function CreateVideoForm() {
-
-    useEffect(() => {
-        validator({
-            form: '#create-video-form',
-            formGroup: '.form-group',
-            errorMessage: '.form-message',
-            styleInvalid: 'border-red-500' ,
-            rules: [
-                validator.isRequired('#title', 'Vui lòng nhập title'),
-
-            ],
-            onsubmit: function (data) {
-                console.log('data',data)
-                ///call api
-            }
-
-        })
-    },[])    
+  useEffect(() => {
+    validator({
+      form: "#create-post-item",
+      formGroup: ".form-group",
+      errorMessage: ".form-message",
+      styleInvalid: "border-red-500",
+      rules: [
+        validator.isRequired("#title", "Vui lòng nhập title"),
+        validator.isRequired("#description", "Vui lòng nhập mo ta"),
+        validator.isRequired("#linkPDF", "Vui lòng nhập linkpdf"),
+      ],
+      onsubmit: function (data) {
+        console.log("data", data);
+      },
+    });
+  }, []);
   return (
-    <form id='create-video-form'>
-        <div className='form-group'>
-            <label htmlFor='title'>Title</label>
-            <input type='text' id='title' name='title' className='form-control' />
-            <div className='form-message'></div>
-        </div>
-        <div className='form-group'>
-            <label htmlFor='description'>Description</label>
-            <textarea id='description' name='description' className='form-control'></textarea>
-            <div className='form-message'></div>
-        </div>
-        <div className='form-group'>
-            <label htmlFor='link'>Link Video: </label>
-            <input type='text' id='link' name='link' className='form-control' />
-            <div className='form-message'></div>
-        </div>
-        <button type='submit' className='btn btn-primary'>Submit</button>
+    <form id="create-post-item">
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" className="form-control" />
+        <div className="form-message"></div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          className="form-control"
+        ></textarea>
+        <div className="form-message"></div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="linkPDF">linkPDF Video: </label>
+        <input
+          type="text"
+          id="linkPDF"
+          name="linkPDF"
+          className="form-control"
+        />
+        <div className="form-message"></div>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
-  )
+  );
 }
