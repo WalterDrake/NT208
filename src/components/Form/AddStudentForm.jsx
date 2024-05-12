350c9630c3f2ea232bb75df20c8efb5deaa29103
nhocratac <22521339@gm.uit.edu.vn>
2024-05-12 22:53:10 +0700

    add student form

--- src/components/Form/AddStudentForm.jsx
+++ src/components/Form/AddStudentForm.jsx
@@ -52,29 +52,31 @@ export default function AddStudentForm({idAdd,isStudy,isCourse}) {
         }
     })
   return (
-<div className="bg-stone-400 min-h-24 w-auto">  
-        <form  id="add-studen-form"> 
-        <div className="form-group">
-            <label htmlFor="student-email">email: </label>
-            <input ref={inputEmailRef} type="email" value={studentEmail} onChange={handleChange} placeholder="@gm.uit.edu.vn" name="studentEmail" id='student-email' className="border-black border-2 w-auto"/>
-            <div className="form-message text-red-700 flex justify-center"></div>
-        </div>
-        <br></br>
-        <input type="submit" value="Thêm sinh viên" className="border-2 border-blue-400 bg-blue-300  rounded-lg hover:bg-blue-500"/>
-        </form>
-        <div>
-            <ul>
-                {StudentEmails.map((email, index) => {
-                    return (
-                        <li key={index} id={`studentList-${index}`}>
-                            <p>{email}</p>
-                        </li>
-                    )
-                })}
-            </ul>
-            <Button onClick={() => setStudentEmails([])}>Xóa tất cả</Button>
-            <Button onClick={handleSubmitAdd}>thêm toàn bộ</Button>
-        </div> 
+<div className='bg-stone-400 min-h-24 w-auto'>
+    <div className=" flex justify-center">  
+            <form  id="add-studen-form"> 
+            <div className="form-group">
+                <label htmlFor="student-email">email: </label>
+                <input ref={inputEmailRef} type="email" value={studentEmail} onChange={handleChange} placeholder="@gm.uit.edu.vn" name="studentEmail" id='student-email' className="border-black border-2 w-auto"/>
+                <div className="form-message text-red-700 flex justify-center"></div>
+            </div>
+            <br></br>
+            <input type="submit" value="Thêm sinh viên" className="border-2 border-blue-400 bg-blue-300  rounded-lg hover:bg-blue-500"/>
+            </form>
+            <div>
+                <ul>
+                    {StudentEmails.map((email, index) => {
+                        return (
+                            <li key={index} id={`studentList-${index}`}>
+                                <p>{email}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div> 
+    </div>
+    <Button onClick={() => setStudentEmails([])}>Xóa tất cả</Button>
+    <Button onClick={handleSubmitAdd}>thêm toàn bộ</Button>
 </div>
   )
 }
