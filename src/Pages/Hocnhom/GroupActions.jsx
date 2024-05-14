8e4e569d7eb60c430a3e20114d58062ab119ea6f
nhocratac <22521339@gm.uit.edu.vn>
2024-05-14 14:03:06 +0700

    -oke create group

--- src/Pages/Hocnhom/GroupActions.jsx
+++ src/Pages/Hocnhom/GroupActions.jsx
@@ -8,6 +8,9 @@ import validator from '../../hook/validate';
 import { Chip } from '@mui/material';
 
 export const CreateGroup = () => {
+  const memberInputRef = useRef()
+  const [memberInput, setMemeberInput] = useState('')
+  const [memberList, setMemeberList] = useState([])
   const { user } = useUser()
   useEffect(() => {
     validator({
@@ -17,6 +20,8 @@ export const CreateGroup = () => {
       styleInvalid: 'border-red-500',
       rules: [
         validator.isRequired('#add-group-name', 'Nhập tên nhóm'),
+        validator.isRequired('#add-group-code', 'Nhập mã code'),
+        validator.isRequired('#add-group-desrciption', 'Nhập mô tả'),
         validator.isEmail1('#add-group-member', 'Email không hợp lệ'),
       ],
       onSubmit: function (data) {
@@ -30,10 +35,7 @@ export const CreateGroup = () => {
           })
       }
     })
-  }, [])
-  const memberInputRef = useRef()
-  const [memberInput, setMemeberInput] = useState('')
-  const [memberList, setMemeberList] = useState([])
+  }, [memberList])
   const addMemeber = (e) => {
     e.preventDefault()
     setMemeberList([...memberList, memberInput])
@@ -53,22 +55,20 @@ export const CreateGroup = () => {
         <div className="border-b bg-white rounded-lg p-4 border-gray-900/10 pb-12 shadow-lg">
           <div className='form-group mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
             <div className="sm:col-span-3">
-              <label htmlFor='group-name' className='block text-ml font-medium leading-6 text-gray-900'>Tên nhóm: </label>
+              <label htmlFor='name' className='block text-ml font-medium leading-6 text-gray-900'>Tên nhóm: </label>
               <div className="mt-2">
                 <input type='text' name='name' id='add-group-name' autoComplete='name' className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' placeholder='Tên nhóm' />
                 <div className='form-message text-red-700 flex justify-center'></div>
-
               </div>
             </div>
           </div>
           <br></br>
 
           <div className='form-group grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
-
             <div className="sm:col-span-3 ">
-              <label htmlFor='group-name' className='block text-ml font-medium leading-6 text-gray-900'>Mã code: </label>
+              <label htmlFor='code' className='block text-ml font-medium leading-6 text-gray-900'>Mã code: </label>
               <div className="mt-2">
-                <input type='text' name='code' id='add-group-name' autoComplete='code' className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' placeholder='Nhập mã code' />
+                <input type='text' name='code' id='add-group-code' autoComplete='code' className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' placeholder='Nhập mã code' />
                 <div className='form-message text-red-700 flex justify-center'></div>
               </div>
             </div>
@@ -76,9 +76,9 @@ export const CreateGroup = () => {
 
           <br></br>
           <div className='form-group col-span-4'>
-            <label htmlFor='group-description' className='block text-ml font-medium leading-6 text-gray-900'>Mô tả: </label>
+            <label htmlFor='description' className='block text-ml font-medium leading-6 text-gray-900'>Mô tả: </label>
             <div className="mt-2">
-              <textarea placeholder='Thêm mô tả' id='add-group-desrciption' name='description' rows="2" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
+              <input type="text" placeholder='Thêm mô tả' id='add-group-desrciption' name='description' rows="2" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
             </div>
             <div className='form-message text-red-700 flex justify-center'></div>
           </div>
@@ -96,6 +96,7 @@ export const CreateGroup = () => {
                     <input name='add-member' id='add-group-member' ref={memberInputRef} value={memberInput} onChange={(e) => setMemeberInput(e.target.value)} type='text' placeholder='Nhập mail sinh viên' className='block flex-1 rounded-md border-0 bg-transparent py-1.5 pl-1 text-gray-900  placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6' />
                     <span className='items-center justify-center p-2'><button onClick={addMemeber} ><PersonAddIcon /></button></span>
                   </div>
+                  <div className='form-message text-red-700 flex justify-center'></div>
                 </div>
               </div>
 
