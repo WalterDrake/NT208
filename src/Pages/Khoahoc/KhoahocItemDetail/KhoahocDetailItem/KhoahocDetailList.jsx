c28b02679feffe33696e22fd257f285ad8f300e6
Sukem <thuytranggd11@gmail.com>
2024-05-23 21:39:55 +0700

    css list video

--- src/Pages/Khoahoc/KhoahocItemDetail/KhoahocDetailItem/KhoahocDetailList.jsx
+++ src/Pages/Khoahoc/KhoahocItemDetail/KhoahocDetailItem/KhoahocDetailList.jsx
@@ -1,21 +1,26 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import * as item from "../../../../service/item";
-
-
 import { CurrentItemContext } from "../../../../state/CoursecDetailProvider";
-
 import useUser from "../../../../hook/useUser";
 import ActionList from "./ActionList";
 import { useParams } from "react-router-dom";
+import { Box, ListItemButton, ListItemText } from "@mui/material";
+import { KeyboardArrowDown } from "@mui/icons-material";
+import ListVideo from "../../../../components/CourseDetail/ListVideo";
 
 export default function KhoahocDetailList() {
+        const [openedItemId, setOpenedItemId] = useState(null); // Use null to represent no open item
         const { user } = useUser();
-        const {setCurItem } = useContext(CurrentItemContext);
-        const {courseId} = useParams()
+        const { setCurItem } = useContext(CurrentItemContext);
+        const { curItem } = useContext(CurrentItemContext);
+        const { courseId } = useParams();
         const [listItem, setListItem] = useState([]);
-        const handleCuritem = (item) => {
-                setCurItem(item)       
-        }
+
+        const handleItemClick = (item) => {
+                setCurItem(item);
+                setOpenedItemId(item.id); // Toggle the open state to the current item's id
+        };
+
         useEffect(() => {
                 item
                         .getListItemOfCourse(courseId)
@@ -27,27 +32,47 @@ export default function KhoahocDetailList() {
                                 console.log("err", err);
                         });
         }, [courseId]);
+
         return (
-                <ul className="h-full overflow-auto">
-                        {listItem.map((item, index) => {
-                                return (
-                                        <li
-                                                key={index}
-                                                className="border-b border-[#ccc]"
+                <Box>
+                        {listItem.map((item, index) => (
+                                <ListItemButton
+                                        key={index}
+                                        alignItems="flex-start"
+                                        onClick={() => handleItemClick(item)}
+                                        sx={{
+                                                px: 3,
+                                                pt: 2.5,
+                                                pb: openedItemId === item.id ? 0 : 2.5, // Conditionally set the bottom padding based on the opened state
+                                                "&:hover, &:focus": { "& svg": { opacity: openedItemId === item.id ? 1 : 0 } },
+                                        }}
+                                >
+                                        <ListItemButton
+                                                sx={{ py: 0, minHeight: 32, color: "black" }}
+                                                onClick={() => handleItemClick(item)}
                                         >
-                                                <div className="flex justify-between items-center p-2 ">
-                                                        <div className='bg-blue-300 w-full rounded-xl border-blue-500 hover:bg-blue-500' onClick={() =>handleCuritem(item)}>
-                                                                <h5> Title : {item.title} </h5>
-                                                                <h5> Description : {item.description} </h5>
-                                                        </div>
-                                                </div>
-                                                <div>
-                                                        <ActionList item={item} user={user}></ActionList>
-                                                </div>
-                                        </li>
-                                );
-                        })}
-                </ul>
+                                                <ListItemText
+                                                        primary={item.title}
+                                                        primaryTypographyProps={{ fontSize: 14, fontWeight: "medium" }}
+                                                />
+                                                <ListItemText
+                                                        primary={item.description}
+                                                        primaryTypographyProps={{ fontSize: 14, fontWeight: "medium" }}
+                                                />
+                                                <ActionList item={item} user={user} />
+                                        </ListItemButton>
+
+                                        <KeyboardArrowDown
+                                                sx={{
+                                                        mr: -1,
+                                                        opacity: openedItemId === item.id ? 1 : 0, // Conditionally set the opacity based on the opened state
+                                                        transform: openedItemId === item.id ? "rotate(-180deg)" : "rotate(0)",
+                                                        transition: "0.2s",
+                                                }}
+                                        />
+                                </ListItemButton>
+                        ))}
+                        {openedItemId !== null && <ListVideo item={curItem} />}
+                </Box>
         );
 }
-
