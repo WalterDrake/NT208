0e050ebed1fe451ac977dae4d35d2926b72953bc
nhocratac <22521339@gm.uit.edu.vn>
2024-05-30 21:39:56 +0700

    start sever

--- src/server.js
+++ src/server.js
@@ -1,71 +1,70 @@
 /* eslint-disable no-console */
-
 import express from "express";
 import cors from "cors";
-import { corsOptions } from "~/config/cors";
+import { corsOptions } from "./config/cors";
 import exitHook from "async-exit-hook";
-import { CONNECT_DB, CLOSE_DB } from "~/config/mongodb";
-import { env } from "~/config/environment";
-import { APIs } from "~/routes/api";
-import { errorHandlingMiddleware } from "~/middlewares/errorHandlingMiddleware";
+import { CONNECT_DB, CLOSE_DB } from "./config/mongodb";
+import { env } from "./config/environment";
+import { APIs } from "./routes/api";
+import { errorHandlingMiddleware } from "./middlewares/errorHandlingMiddleware";
 const { Server } = require("socket.io");
 const http = require("http");
 
 const START_SERVER = () => {
-  const app = express();
+    const app = express();
 
-  // Hanlde cors
-  app.use(cors(corsOptions));
+    // Handle cors
+    app.use(cors(corsOptions));
 
-  const server = http.createServer(app);
+    const server = http.createServer(app);
 
-  const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"],
-    },
-  });
-
-  io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-
-    socket.on("join_room", (data) => {
-      socket.join(data); // tham gia vao cai luong do
+    const io = new Server(server, {
+        cors: {
+            origin: "http://localhost:5173",
+            methods: ["GET", "POST"],
+        },
     });
 
-    socket.on("send_message", (data) => {
-      //
-      socket.to(data.room).emit("receive_message", data);
+    io.on("connection", (socket) => {
+        console.log(`User Connected: ${socket.id}`);
+
+        socket.on("join_room", (data) => {
+            socket.join(data);
+        });
+
+        socket.on("send_message", (data) => {
+            socket.to(data.room).emit("receive_message", data);
+        });
     });
-  });
 
-  // Enable req.body json data
-  app.use(express.json());
+    // Enable req.body json data
+    app.use(express.json());
 
-  // Use APIs V1
-  app.use("/api", APIs);
+    // Use APIs V1
+    app.use("/api", APIs);
 
-  // Middleware handles errors collectively
-  app.use(errorHandlingMiddleware);
+    // Middleware handles errors collectively
+    app.use(errorHandlingMiddleware);
 
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`http://${env.APP_HOST}:${env.APP_PORT}`);
-  });
-  // Clean up task before stopping server
-  exitHook(() => {
-    CLOSE_DB();
-    console.log("Disconnected from server");
-  });
+    server.listen(env.APP_PORT, env.APP_HOST, () => {
+        console.log(`http://${env.APP_HOST}:${env.APP_PORT}`);
+    });
+
+    // Clean up task before stopping server
+    exitHook(() => {
+        CLOSE_DB();
+        console.log("Disconnected from server");
+    });
 };
 
 // Start server back-end when connected database successfully
 (async () => {
-  try {
-    await CONNECT_DB();
-    console.log("Connected to server");
-    START_SERVER();
-  } catch (error) {
-    console.log(error);
-    process.exit(0);
-  }
+    try {
+        await CONNECT_DB();
+        console.log("Connected to server");
+        START_SERVER();
+    } catch (error) {
+        console.log(error);
+        process.exit(0);
+    }
 })();
