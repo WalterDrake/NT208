d7dee0bb835c5d43fd4c7ffc0edaec1cbd5c29c8
Sukem <thuytranggd11@gmail.com>
2024-05-30 23:48:51 +0700

    profile page

--- src/Pages/Profilepage.jsx
+++ src/Pages/Profilepage.jsx
@@ -55,34 +55,40 @@ const Profilepage = () => {
   }, [])
 
   return (
-    <div className='m-28 bg-slate-100 w-full flex-row'>
-      <h1 className='text-4xl font-bold text-center'>Profile Page</h1>
-      <div className='flex justify-center'>
-        <Avatar alt="Remy Sharp" src={avatar} sx={{ width: 90, height: 90 }} />
-      </div>
-      <p>Tên: {user.username}</p>
-      <p>Role: {user.role}</p>
-      <form id='form-profile' onSubmit={handleSubmitProfile}>
-        <div className='profile-form-group flex'>
-          <label htmlFor='avatar' className='p-6 w-30 sm:w-40'>Avatar: </label>
-          <input type='url' name='avatar' placeholder='chosse link iamge' autoComplete='off' value={avatar} className='border-2 border-black flex-1 ' onChange={e => setAvatar(e.target.value)}></input>
+    <div
+      className="form w-[80%] rounded-md shadow-xl overflow-hidden z-[100] relative cursor-pointer snap-start shrink-0 py-5 px-6 bg-white flex flex-col items-center justify-center gap-3 ml-28 transition-all duration-300"
+    >
+      <p
+        className="text-[#A15A3E] translate-x-[46%] -rotate-90 tracking-[20px] transition-all hover:translate-x-[50%] -translate-y-1/2 font-semibold text-2xl absolute right-0"
+      >
+        {user.username}
+      </p>
+      <div className="capitalize  w-[80%]">
+        <p className="text-2xl text-[#7F3D27] text-center font-bold mt-2">Profile page</p>
+        <div className='flex justify-center'>
+          <Avatar alt={user.username} src={avatar} sx={{ width: 100, height: 100 }} />
         </div>
-        <br></br>
-        <>
-          <div className='profile-form-group flex'>
-            <label htmlFor='username' className='p-6 w-30 sm:w-40'>Username: </label>
-            <input type='text' name='username' autoComplete='off' className='border-2 border-black flex-1 ' placeholder='username' value={username} onChange={handleUsername}></input>
+        <form id='form-profile' onSubmit={handleSubmitProfile} className='flex flex-col gap-3'>
+          <div className='profile-form-group flex flex-col items-start w-full'>
+            <label htmlFor='avatar' className='text-sm text-[#7F3D27] font-bold'>Avatar: </label>
+            <input type='url' name='avatar' placeholder='Chosse link iamge' autoComplete='off' value={avatar} className='w-full py-px pl-0 bg-transparent outline-none focus:ring-0 border-0 border-b-2 border-[#7F3D27] placeholder:text-[#A15A3E] focus:outline-none text-[#7F3D27] placeholder:text-xs' onChange={e => setAvatar(e.target.value)}></input>
           </div>
-          <br></br>
-          <div className='profile-form-group flex'>
-            <label htmlFor='password' className='p-6 w-30 sm:w-40'>Password: </label>
-            <input type='password' name='password' className='border-2 border-black flex-1 ' placeholder='nhập password' value={password} onChange={handlePassword}></input>
-          </div>
-        </>
 
-        <input type='submit' value='Thay doi profile' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'></input>
-      </form>
-    </div>
+          <div className='profile-form-group flex flex-col items-start w-full'>
+            <label htmlFor='username' className='text-sm text-[#7F3D27] font-bold'>Username: </label>
+            <input type='text' name='username' autoComplete='off' className='w-full py-px pl-0 bg-transparent outline-none focus:ring-0 border-0 border-b-2 border-[#7F3D27] placeholder:text-[#A15A3E] focus:outline-none text-[#7F3D27] placeholder:text-xs' placeholder='Username' value={username} onChange={handleUsername}></input>
+          </div>
+
+          <div className='profile-form-group flex flex-col items-start w-full'>
+            <label htmlFor='password' className='text-sm text-[#7F3D27] font-bold'>Password: </label>
+            <input type='password' name='password' className='w-full py-px pl-0 bg-transparent outline-none focus:ring-0 border-0 border-b-2 border-[#7F3D27] placeholder:text-[#A15A3E] focus:outline-none text-[#7F3D27] placeholder:text-xs' placeholder='nhập password' value={password} onChange={handlePassword}></input>
+          </div>
+
+
+          <input type='submit' value='Lưu thay đổi' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'></input>
+        </form>
+      </div>
+    </div >
   )
 }
 
