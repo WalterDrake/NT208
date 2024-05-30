4e274354d887c366a034ec68f4c4dc4666d728c1
Sukem <thuytranggd11@gmail.com>
2024-05-30 22:06:54 +0700

    sửa put

--- src/routes/api/teacherRoute.js
+++ src/routes/api/teacherRoute.js
@@ -16,7 +16,7 @@ Router.route("/TeacherLogin/:email/:password").post(
   teacherController.TeacherLogin
 );
 Router.route("/Teacher/:id").get(teacherController.getTeacherDetails);
-Router.route("/UpdateTeacher/:id").get(teacherController.updateTeacher);
+Router.route("/UpdateTeacher/:id").put(teacherController.updateTeacher);
 
 Router.route("/getall").get(teacherController.getTeacherAll);
 // API hỗ trợ việc di chuyển card giữa các column khác nhau trong một board
--- src/routes/api/userRoute.js
+++ src/routes/api/userRoute.js
@@ -19,6 +19,6 @@ Router.route("/StudentLogin/:email/:password").post(
 );
 Router.route("/StudentLogOut/:id").put(userController.StudentLogOut);
 Router.route("/getAlluserOnline").get(userController.getAllUserOnline);
-Router.route("/UpdateStudent/:id").get(userController.updateStudent);
+Router.route("/UpdateStudent/:id").put(userController.updateStudent);
 
 export const userRoute = Router;
