7b8218c26b3a90be68d0887a9712b8e637ca0c3a
Hồ Đăng Mạnh Hưng <117001966+manhhunghoo@users.noreply.github.com>
2024-05-30 23:04:04 +0700

    update

--- src/models/RoomChat/connectChat.js
+++ src/models/RoomChat/connectChat.js
@@ -1,11 +1,20 @@
+import { messageModel } from "../Hocnhom/ChatRealTime/messageModel";
+import { messengerModel } from "./messengetModel";
+
 function connectChat(io) {
   io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
     socket.on("join_room", (data) => {
       socket.join(data);
+      //Lay du lieu
+      socket
+        .to(data.code)
+        .emit("receive_message", messengerModel.getAllChatOfGroup(data.code));
     });
     socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);  // data :" userid , code  , message, username,linkimage"
+      socket.to(data.code).emit("receive_message", data); // data :" userid , code  , message, username,linkimage"
+      //Luu thong tin tin nhan
+      messengerModel.createMessage(data);
     });
   });
 }
--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const START_SERVER = () => {
   const server = http.createServer(app);
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5170",
+      origin: "http://localhost:5173",
       methods: ["GET", "POST"],
     },
   });
