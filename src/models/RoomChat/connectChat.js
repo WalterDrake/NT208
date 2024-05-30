0b13f721d79549803b67a5da7b05ec68322b67d4
nhocratac <22521339@gm.uit.edu.vn>
2024-05-31 01:24:16 +0700

    -update

--- src/models/RoomChat/connectChat.js
+++ src/models/RoomChat/connectChat.js
@@ -4,16 +4,6 @@ function connectChat(io) {
   io.on("connection", (socket) => {
     socket.on("join_room", (data) => {
       socket.join(data);
-      messengerModel.getAllChatOfGroup(data) 
-      .then((array) => {
-        console.log(array)
-        socket
-        .to(data)
-         .emit("join_room_res",array);
-      })
-      // socket
-      //   .to(data)
-      //   .emit("join_room_res", messengerModel.getAllChatOfGroup(data));
     });
 
     socket.on("send_message", (data) => {
