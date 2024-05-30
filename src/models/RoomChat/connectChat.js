2dd45e61bcc2a2b562fe723949bc4df7cd2949f1
Hồ Đăng Mạnh Hưng <117001966+manhhunghoo@users.noreply.github.com>
2024-05-30 23:18:31 +0700

    update config chat

--- src/models/RoomChat/connectChat.js
+++ src/models/RoomChat/connectChat.js
@@ -1,11 +1,18 @@
+import { messageModel } from "../Hocnhom/ChatRealTime/messageModel";
+import { messengerModel } from "./messengetModel";
+
 function connectChat(io) {
   io.on("connection", (socket) => {
     socket.on("join_room", (data) => {
       socket.join(data);
+      socket
+        .to(data.code)
+        .emit("join_room_res", messengerModel.getAllChatOfGroup(data.code));
     });
 
     socket.on("send_message", (data) => {
       socket.to(data.code).emit("receive_message", data);
+      messengerModel.createMessage(data);
     });
   });
 }
