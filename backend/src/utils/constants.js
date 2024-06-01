c2e6e1974de690f7032da16f8aaee81d0d412e65
Hồ Đăng Mạnh Hưng <117001966+manhhunghoo@users.noreply.github.com>
2024-06-01 19:56:09 +0700

    update

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -2,6 +2,7 @@
 export const WHITELIST_DOMAINS = [
   "https://uitwebelearning.onrender.com",
   "http://localhost:5173",
+  "https://uitwebelearning-j1slzcxs0-hodangmanhhungs-projects.vercel.app",
 ];
 
 export const BOARD_TYPES = {
