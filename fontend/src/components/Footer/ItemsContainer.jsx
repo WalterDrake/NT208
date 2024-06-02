fe1621224437957491178d0377c63170f7e249db
Sukem <thuytranggd11@gmail.com>
2024-06-02 23:31:48 +0700

    lỗi ko hiện picture

--- fontend/src/components/Footer/ItemsContainer.jsx
+++ fontend/src/components/Footer/ItemsContainer.jsx
@@ -1,11 +1,12 @@
 
 import Item from "./Item";
 import { RESOURCES } from "./Menus";
+import LogoUIT from "../../assets/LogoUIT.svg"
 const ItemsContainer = () => {
     return (
         <div className="flex justify-center lg:grid-cols-2 gap-6 sm:px-8 px-52 py-8">
             <img
-                src="./src/assets/LogoUIT.svg"
+                src={LogoUIT}
                 alt="logoUIT"
             />
             <Item Links={RESOURCES} />
